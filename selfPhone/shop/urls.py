cd1dc7ad3476723f8d032464150ae0b7dd462082
Thomas <Thomas@Tannenberg.online>
2024-04-15 13:20:42 +0200

    integrated basic JS structure

--- selfPhone/shop/urls.py
+++ selfPhone/shop/urls.py
@@ -3,4 +3,17 @@ from . import views
 
 urlpatterns = [
     path('', views.shop, name='shop'),
+
+    path('start/', views.start, name='start'),
+
+    path('product_gallery/', views.product_gallery, name='product_gallery'),
+
+    path('product_details/', views.product_details, name='product_details'),
+
+    path('basket/', views.basket, name='basket'),
+
+    path('checkout/', views.checkout, name='checkout'),
+
+    path('login/', views.login, name='login'),
+
 ]
--- selfPhone/shop/views.py
+++ selfPhone/shop/views.py
@@ -5,3 +5,27 @@ from django.shortcuts import render
 
 def shop(request):
     return render(request, 'shop/index.html')
+
+
+def start(request):
+    return render(request, 'shop/start.html')
+
+
+def product_gallery(request):
+    return render(request, 'shop/product_gallery.html')
+
+
+def product_details(request):
+    return render(request, 'shop/product_details.html')
+
+
+def basket(request):
+    return render(request, 'shop/basket.html')
+
+
+def checkout(request):
+    return render(request, 'shop/checkout.html')
+
+
+def login(request):
+    return render(request, 'shop/login.html')
